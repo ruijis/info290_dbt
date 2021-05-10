374dbfc022f43eb1dfac14ad7edd0aaa15edd2e5
Yash Baldawa <32589573+yb4@users.noreply.github.com>
2021-05-09 21:12:08 -0700

    new second model added

---
+++ models/my_second_model.sql
@@ -0,0 +1,22 @@
+
+/*
+    Welcome to your first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+
+    Try changing "table" to "view" below
+*/
+
+{{ config(materialized='table') }}
+
+
+
+SELECT buildings_site_mapping.*
+FROM buildings_site_mapping, {{ref('my_first_dbt_model')}} as b
+WHERE buildings_site_mapping.site = b.site
+
+/*
+    Uncomment the line below to remove records with null `id` values
+*/
+
+-- where id is not null
